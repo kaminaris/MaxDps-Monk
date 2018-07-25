68063f8273a2bbecf74d417e8b25cbcc1d5512fe
Pawel <zacdbg@gmail.com>
2018-07-25 21:49:58 +0200

    v8.0.0 - Beta WW

--- main.lua
+++ main.lua
@@ -1,155 +1,145 @@
-﻿-- Spells
-local _KegSmash = 121253;
-local _Jab = 100780;
-local _ExpelHarm = 115072;
-local _TigerPalm = 100787;
-local _ElusiveBrew = 115308;
-local _PurifyingBrew = 119582;
-local _BlackoutKick = 100784;
-local _ChiExplosion = 157676;
+﻿--- @type MaxDps
+if not MaxDps then
+	return;
+end
+
+local Monk = MaxDps:NewModule('Monk');
 
 -- Auras
-local _HeavyStagger = 124273;
-local _ModerateStagger = 124274;
-local _Shuffle = 115307;
-local _ElusiveBrewAura = 128939;
-local _TigerPower = 125359;
-
--- New
-local _FistsofFury = 113656;
-local _StrikeoftheWindlord = 205320;
-local _WhirlingDragonPunch = 152175;
-local _TigerPalm = 100780;
-local _RisingSunKick = 107428;
-local _ChiWave = 115098;
-local _BlackoutKick = 100784;
+local _HitComboAura = 196741;
 local _BlackoutKickAura = 116768;
+local _RushingJadeWindAura = 148187;
+
+-- Spells
+local _TigerPalm = 100780;
+local _FistsofFury = 113656;
+local _RisingSunKick = 107428;
+local _BlackoutKick = 100784;
 local _MarkoftheCrane = 228287;
 local _SpinningCraneKick = 101546;
 local _StormEarthandFire = 137639;
-local _Serenity = 152173;
-local _RushingJadeWind = 116847;
-local _ChiBurst = 123986;
 local _TouchofDeath = 115080;
-local _Ascension = 115396;
+local _TouchofKarma = 122470;
 local _MasteryComboStrikes = 115636;
 local _Afterlife = 116092;
-local _HealingSphere = 125355;
-local _TouchofKarma = 122470;
-local _EnergizingElixir = 115288;
-local _Roll = 109132;
-local _Celerity = 115173;
-local _HitCombo = 196740;
-local _HitComboAura = 196741;
 local _Transcendence = 101643;
 local _TranscendenceTransfer = 119996;
 
-local _HitComboAbilities = {
-	[_BlackoutKick] = 1,
-	[_ChiWave] = 1,
-	[_FistsofFury] = 1,
-	[_RisingSunKick] = 1,
-	[_TigerPalm] = 1,
-	[_TouchofDeath] = 1,
-	[_StrikeoftheWindlord] = 1,
-}
 
-MaxDps.Monk = {};
+-- Talents
+local _ChiWave = 173527;
+local _ChiBurst = 123986;
+local _Ascension = 115396;
+local _FistOfTheWhiteTiger = 261947;
+local _HitCombo = 196740;
+local _Xuen = 123904;
+local _WhirlingDragonPunch = 152175;
+local _Serenity = 152173;
+local _RushingJadeWind = 261715;
 
-function MaxDps.Monk.CheckTalents()
+
+function Monk:Enable()
+	MaxDps:Print(MaxDps.Colors.Info .. 'Monk [Brewmaster, Mistweaver, Windwalker]');
+
+	if MaxDps.Spec == 1 then
+		MaxDps.NextSpell = Monk.Brewmaster;
+	elseif MaxDps.Spec == 2 then
+		MaxDps.NextSpell = Monk.Mistweaver;
+	elseif MaxDps.Spec == 3 then
+		MaxDps.NextSpell = Monk.Windwalker;
+	end;
+
+	return true;
 end
 
-function MaxDps:EnableRotationModule(mode)
-	mode = mode or 1;
-	self.Description = 'Monk [Windwalker]';
-	self.ModuleOnEnable = MaxDps.Monk.CheckTalents;
-	if mode == 1 then
-		self.NextSpell = MaxDps.Monk.Brewmaster;
-	end;
-	if mode == 2 then
-		self.NextSpell = MaxDps.Monk.Mistweaver;
-	end;
-	if mode == 3 then
-		self.NextSpell = MaxDps.Monk.Windwalker;
-	end;
-	self:RegisterEvent('UNIT_SPELLCAST_SUCCEEDED');
-	self.lastSpellId = 0;
-end
 
-function MaxDps:UNIT_SPELLCAST_SUCCEEDED(event, unitID, spell, rank, lineID, spellID)
-	if unitID == 'player' and _HitComboAbilities[spellID] == 1 then
-		self.lastSpellId = spellID;
-	end
-end
 
-function MaxDps.Monk.Brewmaster(_, timeShift, currentSpell, gcd, talents)
+function Monk:Brewmaster(timeShift, currentSpell, gcd, talents)
 	return nil;
 end
 
-function MaxDps.Monk.Mistweaver(_, timeShift, currentSpell, gcd, talents)
+function Monk:Mistweaver(timeShift, currentSpell, gcd, talents)
 	return nil;
 end
 
-function MaxDps.Monk.Windwalker(_, timeShift, currentSpell, gcd, talents)
-	local chi = UnitPower('player', SPELL_POWER_CHI);
-	local energy = UnitPower('player', SPELL_POWER_ENERGY);
-	local energyMax = UnitPowerMax('player', SPELL_POWER_ENERGY);
-
-	local hit, hitCharges = MaxDps:Aura(_HitComboAura, timeShift);
-
-	local rsk = MaxDps:SpellAvailable(_RisingSunKick, timeShift);
-	local fotf, fotfCd = MaxDps:SpellAvailable(_FistsofFury, timeShift);
-
-	if talents[_StormEarthandFire] then
-		MaxDps:GlowCooldown(_StormEarthandFire, MaxDps:SpellAvailable(_StormEarthandFire, timeShift));
-	end
+function Monk:Windwalker(timeShift, currentSpell, gcd, talents)
+	-- Cooldowns
 
 	if talents[_Serenity] then
 		MaxDps:GlowCooldown(_Serenity, MaxDps:SpellAvailable(_Serenity, timeShift));
+	else
+		MaxDps:GlowCooldown(_StormEarthandFire, MaxDps:SpellAvailable(_StormEarthandFire, timeShift));
+	end
+
+	if talents[_Xuen] then
+		MaxDps:GlowCooldown(_Xuen, MaxDps:SpellAvailable(_Xuen, timeShift));
 	end
 
 	MaxDps:GlowCooldown(_TouchofDeath, MaxDps:SpellAvailable(_TouchofDeath, timeShift));
+	MaxDps:GlowCooldown(_TouchofKarma, MaxDps:SpellAvailable(_TouchofKarma, timeShift));
 
-	if talents[_WhirlingDragonPunch]
-		and not fotf
-		and not rsk
-		and MaxDps:SpellAvailable(_WhirlingDragonPunch, timeShift)
-	then
-		return _WhirlingDragonPunch;
-	end
+	-- Auras
 
+	local hit, hitCharges = MaxDps:Aura(_HitComboAura, timeShift);
+
+	-- CD Checkers
+
+	local rsk = MaxDps:SpellAvailable(_RisingSunKick, timeShift);
+	local fotf, fotfCd = MaxDps:SpellAvailable(_FistsofFury, timeShift + 0.5);
+	local wdp, wdpCd = MaxDps:SpellAvailable(_WhirlingDragonPunch, timeShift);
+
+	local chi = UnitPower('player', Enum.PowerType.Chi);
+	local energy = UnitPower('player', Enum.PowerType.Energy);
+	local energyMax = UnitPowerMax('player', Enum.PowerType.Energy);
+
+	-- Rotation start
 	if fotf and chi >= 3 then
 		return _FistsofFury;
 	end
 
-	if MaxDps:SpellAvailable(_StrikeoftheWindlord, timeShift) and chi >= 2 then
-		return _StrikeoftheWindlord;
+	if talents[_FistOfTheWhiteTiger]
+		and MaxDps:SpellAvailable(_FistOfTheWhiteTiger, timeShift)
+		and energy >= 40
+		and chi < 3
+	then
+		return _FistOfTheWhiteTiger;
 	end
 
-	if (chi < 4 and (energyMax - energy < 20)) and (not hit or MaxDps.lastSpellId ~= _TigerPalm) then
-		return _TigerPalm;
-	end
-
-	if (rsk and chi >= 2) and (not hit or MaxDps.lastSpellId ~= _RisingSunKick) then
+	if talents[_WhirlingDragonPunch]
+		and wdpCd <= 2
+		and MaxDps:SpellAvailable(_RisingSunKick, timeShift)
+		and chi >= 2
+	then
 		return _RisingSunKick;
 	end
 
-	if talents[_ChiWave] and MaxDps:SpellAvailable(_ChiWave, timeShift) then
-		return _ChiWave;
+	if talents[_WhirlingDragonPunch]
+		and wdp
+		and not rsk
+		and not fotf
+	then
+		return _WhirlingDragonPunch;
 	end
 
-	local canBlackout = MaxDps:Aura(_BlackoutKickAura, timeShift) or
-			(MaxDps:SpellAvailable(_BlackoutKick, timeShift) and chi > 0);
-	if
-		(canBlackout and (chi > 3 or fotfCd > 3))
-		and (not hit or MaxDps.lastSpellId ~= _BlackoutKick)
+	if (chi < 4 and (energyMax - energy < 20)) then
+		return _TigerPalm;
+	end
+
+	if talents[_ChiBurst]
+		and MaxDps:SpellAvailable(_ChiBurst, timeShift)
+		and chi < 5
+		and currentSpell ~= _ChiBurst
 	then
+		return _ChiBurst;
+	end
+
+	if (chi >= 1 and fotfCd > 2) or MaxDps:Aura(_BlackoutKickAura, timeShift) then
 		return _BlackoutKick;
 	end
 
-	if hit and MaxDps.lastSpellId == _TigerPalm then
-		return nil;
-	else
-		return _TigerPalm;
+	if MaxDps:SpellAvailable(_RisingSunKick, timeShift) and fotfCd > 2 and chi >= 2 then
+		return _RisingSunKick;
 	end
+
+	return _TigerPalm
 end
